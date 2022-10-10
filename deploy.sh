4efe9d6a3d053076504b08b0d1a2f2b37979d503
Korben21 <mani77930@gmail.com>
2022-10-10 21:23:16 +0200

    add script sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#####################################################################
+#
+# Description : Déploiement à la volée de conteneur docker
+#
+# Auteur      : Stephen CASSE
+#
+# Date        : 10/10/2022
+#
+# Version     : v1.0
+#
+#####################################################################
+
+
+echo "
+
+Options :
+   - --create : Lancer des conteneurs
+
+   - --drop : supprimer les conteneurs créer par le deploy.sh
+
+   - --infos : caractéristiques des conteneurs (ip, nom, user...)
+
+   - --start : redémarrage des conteneurs
+
+   - --ansible : déploiement arborescence ansible
+
+"
